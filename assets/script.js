751dcc59230e6e7b648c3fa4323df649bbffcf00
Abuoblie <Abu.oblie@gmail.com>
2021-03-09 16:10:00 +0100

    graph2

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+let randomColor = () => {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    return color;
+}
 
 let graphOne = () => {
 
@@ -36,22 +43,252 @@ let graphOne = () => {
 graphOne();
 
 //Graph 2
+const label = [];
+const data2 = {};
+const year = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
+let table1 = document.getElementById("table1");
+table = Array.from(tableToJson(table1));
+for (let j = 1; j < table.length; j++) {
+    let sum = [];
+
+    for (let i = 1; i < table[j].length; i++) {
+
+        if (parseFloat(table[j][i])) {
+
+            sum.push(parseFloat(table[j][i]));
+        }
+
+    }
+    data2[table[j][0]] = sum;
+}
+const values = Object.values(data2).map(e => e.reduce((acc, val) => acc + parseFloat(val), 0));
+console.log(Object.keys(data2)[1]);
+
 const ctxCrime = document.getElementById('crime').getContext('2d');
 const chartCrime = new Chart(ctxCrime, {
 
     //The type of chart we want to create
     type: 'line',
 
-    //The data for our dataset
+    //The data for our dataset Object.keys(data2)
 
     data: {
-        labels: ["2002", "2012"],
-        datasets: [{
-            label: 'Data',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+        labels: year,
+        datasets: [
+            {
+                label: Object.keys(data2)[0],
+                borderColor: randomColor(),
+                data: Object.values(data2)[0]
 
-        }]
+            },
+            {
+                label: Object.keys(data2)[1],
+                borderColor: randomColor(),
+                data: Object.values(data2)[1]
+
+            },
+            {
+                label: Object.keys(data2)[2],
+                borderColor: randomColor(),
+                data: Object.values(data2)[2]
+
+            },
+            {
+                label: Object.keys(data2)[3],
+                borderColor: randomColor(),
+                data: Object.values(data2)[3]
+
+            },
+            {
+                label: Object.keys(data2)[4],
+                borderColor: randomColor(),
+                data: Object.values(data2)[4]
+
+            },
+            {
+                label: Object.keys(data2)[5],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[5]
+
+            },
+            {
+                label: Object.keys(data2)[6],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[6]
+
+            },
+            {
+                label: Object.keys(data2)[7],
+                borderColor: 'rgb(255, 99, 132)',
+                data: Object.values(data2)[7]
+
+            },
+            {
+                label: Object.keys(data2)[8],
+                borderColor: randomColor(),
+                data: Object.values(data2)[8]
+
+            },
+            {
+                label: Object.keys(data2)[9],
+                borderColor: randomColor(),
+                data: Object.values(data2)[9]
+
+            },
+            {
+                label: Object.keys(data2)[10],
+                borderColor: randomColor(),
+                data: Object.values(data2)[10]
+
+            },
+            {
+                label: Object.keys(data2)[11],
+                borderColor: randomColor(),
+                data: Object.values(data2)[11]
+
+            },
+            {
+                label: Object.keys(data2)[12],
+                borderColor: randomColor(),
+                data: Object.values(data2)[12]
+
+            },
+            {
+                label: Object.keys(data2)[13],
+                borderColor: randomColor(),
+                data: Object.values(data2)[13]
+
+            },
+            {
+                label: Object.keys(data2)[14],
+                borderColor: randomColor(),
+                data: Object.values(data2)[14]
+
+            },
+            {
+                label: Object.keys(data2)[15],
+                borderColor: randomColor(),
+                data: Object.values(data2)[15]
+
+            },
+            {
+                label: Object.keys(data2)[16],
+                borderColor: randomColor(),
+                data: Object.values(data2)[16]
+
+            },
+            {
+                label: Object.keys(data2)[17],
+                borderColor: randomColor(),
+                data: Object.values(data2)[17]
+
+            },
+            {
+                label: Object.keys(data2)[18],
+                borderColor: randomColor(),
+                data: Object.values(data2)[18]
+
+            },
+            {
+                label: Object.keys(data2)[19],
+                borderColor: randomColor(),
+                data: Object.values(data2)[19]
+
+            },
+            {
+                label: Object.keys(data2)[20],
+                borderColor: randomColor(),
+                data: Object.values(data2)[20]
+
+            },
+            {
+                label: Object.keys(data2)[21],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[21]
+
+            },
+            {
+                label: Object.keys(data2)[22],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[22]
+
+            },
+            {
+                label: Object.keys(data2)[23],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[23]
+
+            },
+            {
+                label: Object.keys(data2)[24],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[24]
+
+            },
+            {
+                label: Object.keys(data2)[25],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[25]
+
+            },
+            {
+                label: Object.keys(data2)[26],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[26]
+
+            },
+            {
+                label: Object.keys(data2)[27],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[27]
+
+            },
+            {
+                label: Object.keys(data2)[28],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[28]
+
+            },
+            {
+                label: Object.keys(data2)[29],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[29]
+
+            },
+            {
+                label: Object.keys(data2)[30],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[30]
+
+            },
+            {
+                label: Object.keys(data2)[31],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[31]
+
+            },
+            {
+                label: Object.keys(data2)[32],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[32]
+
+            },
+            {
+                label: Object.keys(data2)[33],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[33]
+
+            },
+            {
+                label: Object.keys(data2)[34],
+                borderColor:  randomColor(),
+                data: Object.values(data2)[34]
+
+            }
+
+
+        ]
+
     },
 
 
@@ -104,24 +341,7 @@ function tableToJson(table) {
     }
     return data;
 }
-let table1D = [];
-let table1 = document.getElementById("table1");
-table = tableToJson(table1);
 
-Array.from(table).forEach(element => {
-    let sum = 0;
-    for (let i = 1; i < element.length; i++) {
-        if (parseFloat(element[i])) {
-            sum += parseFloat(element[i]);
-        }
-
-    }
-    table1D.push([element[0], sum])
-    //console.log(element[0]);
-
-
-
-})
 const table2007_2009 = [];
 const table2010_2012 = [];
 let table2 = document.getElementById("table2");
@@ -132,19 +352,12 @@ Array.from(table2).forEach((element) => {
     table2010_2012.push([element[0], element[1]]);
 
 })
-console.log(table2010_2012);
+
 
 
 
 
 
 //get Random color
-let randomColor = () => {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
-    return color;
-}
 
 
